1567a4036540378974cee98c029af5446df80fc0
Danugnk <danuagungp09@gmail.com>
2025-10-05 16:24:37 +0700

    Add files via upload

---
+++ Prak2/jsonHTML.php
@@ -0,0 +1,35 @@
+<?php
+function curl($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+
+// Ambil data JSON dari file getWisata.php
+$send = curl("http://localhost/getWisata.php");
+
+// Mengubah JSON menjadi array
+$data = json_decode($send, TRUE);
+
+// Membuat tabel HTML
+echo "<table border='1' cellpadding='10' cellspacing='0'>";
+echo "<tr>
+        <th>KOTA</th>
+        <th>LANDMARK</th>
+        <th>TARIF</th>
+      </tr>";
+
+// Menampilkan data setiap baris ke dalam tabel
+foreach($data as $row){
+    echo "<tr>";
+    echo "<td>".$row['kota']."</td>";
+    echo "<td>".$row['landmark']."</td>";
+    echo "<td>".$row['tarif']."</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+?>
